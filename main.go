1967d0e32737c7a21afad5664c0129f80982955c
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-08 16:20:45 +0700

    add aws session

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	_AuthRepo "final-project/repositories/auth"
 	_ServiceRepo "final-project/repositories/service"
 	_UserRepo "final-project/repositories/user"
+	awss3 "final-project/services/aws-s3"
 	"final-project/utils"
 	"fmt"
 
@@ -24,11 +25,11 @@ func main() {
 	userRepo := _UserRepo.NewUserRepository(db)
 	serviceRepo := _ServiceRepo.NewServiceRepository(db)
 
-	// awsSess := awss3.InitS3(config.S3_KEY, config.S3_SECRET, config.S3_REGION)
+	awsSess := awss3.InitS3(config.S3_KEY, config.S3_SECRET, config.S3_REGION)
 
 	ac := _AuthController.NewAuthController(authRepo)
 	uc := _UserController.NewUserController(userRepo)
-	sc := _ServiceController.NewServiceController(serviceRepo)
+	sc := _ServiceController.NewServiceController(serviceRepo, config, awsSess)
 
 	e := echo.New()
 

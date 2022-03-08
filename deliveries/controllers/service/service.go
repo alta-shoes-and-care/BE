5a801603d0f0cbdc1ba90dd6b8910c3ef51222b6
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-08 16:26:09 +0700

    add upload and update image service

--- deliveries/controllers/service/service.go
+++ deliveries/controllers/service/service.go
@@ -1,36 +1,58 @@
 package service
 
 import (
+	"final-project/configs"
 	"final-project/deliveries/controllers/common"
+	"final-project/deliveries/helpers/uploader"
 	"final-project/deliveries/middlewares"
 	_ServiceRepo "final-project/repositories/service"
 	"net/http"
 	"strconv"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 type ServiceController struct {
-	repo _ServiceRepo.Service
+	repo   _ServiceRepo.Service
+	config *configs.AppConfig
+	sess   *session.Session
 }
 
-func NewServiceController(repository _ServiceRepo.Service) *ServiceController {
+func NewServiceController(repository _ServiceRepo.Service, config *configs.AppConfig, awsSess *session.Session) *ServiceController {
 	return &ServiceController{
-		repo: repository,
+		repo:   repository,
+		sess:   awsSess,
+		config: config,
 	}
 }
 
 func (ctl *ServiceController) Create() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var newService RequestCreate
-		var image string
-		userID := middlewares.ExtractTokenUserID(c)
+		isAdmin := middlewares.ExtractTokenIsAdmin(c)
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.UnAuthorized("missing or malformed JWT"))
+		}
 
+		var newService RequestCreate
 		if err := c.Bind(&newService); err != nil || strings.TrimSpace(newService.Title) == "" || strings.TrimSpace(newService.Description) == "" || newService.Price == 0 {
 			return c.JSON(http.StatusBadRequest, common.BadRequest("input dari user tidak sesuai, title, description, atau price tidak boleh kosong"))
 		}
 
+		file, err := c.FormFile("file")
+		if err != nil {
+			log.Info(err)
+			return c.JSON(http.StatusBadRequest, common.BadRequest("tidak dapat membaca file gambar"))
+		}
+
+		image, err := uploader.Uploader(ctl.sess, ctl.config.S3_REGION, file)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, common.BadRequest(err.Error()))
+		}
+
+		userID := middlewares.ExtractTokenUserID(c)
 		res, err := ctl.repo.Create(newService.ToEntityService(image, userID))
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, common.InternalServerError(err.Error()))
@@ -69,16 +91,42 @@ func (ctl *ServiceController) Update() echo.HandlerFunc {
 		}
 
 		var updateService RequestUpdate
-		var image string
 		if err := c.Bind(&updateService); err != nil {
 			return c.JSON(http.StatusBadRequest, common.BadRequest("input dari user tidak sesuai"))
 		}
 
-		res, err := ctl.repo.Update(updateService.ToEntityService(image))
+		res, err := ctl.repo.Update(updateService.ToEntityService())
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, common.InternalServerError(err.Error()))
 		}
 		return c.JSON(http.StatusCreated, common.Success(http.StatusCreated, "sukses memperbarui data service", ToResponseUpdate(res)))
+	}
+}
+
+func (ctl *ServiceController) UpdateImage() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		isAdmin := middlewares.ExtractTokenIsAdmin(c)
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.UnAuthorized("missing or malformed JWT"))
+		}
+
+		file, err := c.FormFile("file")
+		if err != nil {
+			log.Info(err)
+			return c.JSON(http.StatusBadRequest, common.BadRequest("tidak dapat membaca file gambar"))
+		}
+
+		image, err := uploader.Uploader(ctl.sess, ctl.config.S3_REGION, file)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, common.BadRequest(err.Error()))
+		}
+
+		ID, _ := strconv.Atoi(c.Param("id"))
+		res, err := ctl.repo.UpdateImage(uint(ID), image)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError(err.Error()))
+		}
+		return c.JSON(http.StatusCreated, common.Success(http.StatusCreated, "sukses memperbarui gambar service", ToResponseUpdate(res)))
 	}
 }
 

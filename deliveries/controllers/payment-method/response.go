8c64216926cdac96e825d4910fa04e3c5d8fdc8e
fransihsan <fransihsan8888@gmail.com>
2022-03-08 21:17:28 +0700

    initial controller payment method

---
+++ deliveries/controllers/payment-method/response.go
@@ -0,0 +1,27 @@
+package paymentmethod
+
+import PM "final-project/entities/payment-method"
+
+type ResponseCreatePaymentMethod struct {
+	Name    string `json:"name"`
+}
+
+func ToResponseCreatePaymentMethod(PaymentMethod PM.PaymentMethods) ResponseCreatePaymentMethod {
+	return ResponseCreatePaymentMethod{
+		Name:    PaymentMethod.Name,
+	}
+}
+
+type ResponseGetPaymentMethod struct {
+	ID      uint   `json:"id"`
+	Name    string `json:"name"`
+}
+
+func ToResponseGetPaymentMethod(PaymentMethod []PM.PaymentMethods) []ResponseGetPaymentMethod {
+	responses := make([]ResponseGetPaymentMethod, len(PaymentMethod))
+	for i := 0; i < len(PaymentMethod); i++ {
+		responses[i].ID = PaymentMethod[i].ID
+		responses[i].Name = PaymentMethod[i].Name
+	}
+	return responses
+}

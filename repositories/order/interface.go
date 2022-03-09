4f33b75fa062499299caf23916eed416d0720459
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-10 00:55:28 +0700

    rename from response order to format order

--- repositories/order/interface.go
+++ repositories/order/interface.go
@@ -3,16 +3,16 @@ package order
 import O "final-project/entities/order"
 
 type Order interface {
-	Create(newOrder O.Orders) (ResponseOrder, error)
-	Get() ([]ResponseOrder, error)
-	GetByUserID(UserID uint) ([]ResponseOrder, error)
-	GetByID(ID uint) (ResponseOrder, error)
-	InsertUrl(ID uint, url string) (ResponseOrder, error)
-	SetPaid(ID uint) (ResponseOrder, error)
-	SetAccepted(ID uint) (ResponseOrder, error)
-	SetRejected(ID uint) (ResponseOrder, error)
-	SetOnProcess(ID uint) (ResponseOrder, error)
-	SetDelivering(ID uint) (ResponseOrder, error)
-	SetCancel(ID uint) (ResponseOrder, error)
-	SetDone(ID uint) (ResponseOrder, error)
+	Create(newOrder O.Orders) (FormatOrder, error)
+	Get() ([]FormatOrder, error)
+	GetByUserID(UserID uint) ([]FormatOrder, error)
+	GetByID(ID uint) (FormatOrder, error)
+	InsertUrl(ID uint, url string) (FormatOrder, error)
+	SetPaid(ID uint) (FormatOrder, error)
+	SetAccepted(ID uint) (FormatOrder, error)
+	SetRejected(ID uint) (FormatOrder, error)
+	SetOnProcess(ID uint) (FormatOrder, error)
+	SetDelivering(ID uint) (FormatOrder, error)
+	SetCancel(ID uint) (FormatOrder, error)
+	SetDone(ID uint) (FormatOrder, error)
 }

e3a0a075d2f19e14ee0aa524f8e24c767580d667
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-09 21:02:34 +0700

    order repository done

--- repositories/order/order.go
+++ repositories/order/order.go
@@ -52,3 +52,110 @@ func (repo *OrderRepository) GetByUserID(UserID uint) ([]ResponseOrder, error) {
 	}
 	return orders, nil
 }
+
+func (repo *OrderRepository) GetByID(ID uint) (ResponseOrder, error) {
+	var order ResponseOrder
+
+	if rowsAffected := repo.db.Table("orders as o").Select("o.id as ID, o.user_id as UserID, o.service_id as ServiceID, s.service_title as ServiceTitle, s.price as Price, o.qty as Qty, o.date as Date, o.address as Address, o.city as City, o.phone as Phone, o.status as Status, o.is_paid as IsPaid, o.url as Url").Joins("inner join services as s.id = o.service_id").Joins("inner join payment_methods as pm on pm.id = o.payment_method_id").Where("id = ?", ID).First(&order).RowsAffected; rowsAffected == 0 {
+		return ResponseOrder{}, errors.New("gagal mendapatkan detail order")
+	}
+	return order, nil
+}
+
+func (repo *OrderRepository) SetPaid(ID uint) (ResponseOrder, error) {
+	var order ResponseOrder
+
+	if rowsAffected := repo.db.Table("orders").Where("id = ?", ID).Update("is_paid", true).RowsAffected; rowsAffected == 0 {
+		return ResponseOrder{}, errors.New("gagal mengubah status pembayaran menjadi paid")
+	}
+
+	repo.db.Table("orders as o").Select("o.id as ID, o.user_id as UserID, o.service_id as ServiceID, s.service_title as ServiceTitle, s.price as Price, o.qty as Qty, o.date as Date, o.address as Address, o.city as City, o.phone as Phone, o.status as Status, o.is_paid as IsPaid, o.url as Url").
+		Joins("inner join services as s.id = o.service_id").
+		Joins("inner join payment_methods as pm on pm.id = o.payment_method_id").
+		Where("id = ?", ID).First(&order)
+	return order, nil
+}
+
+func (repo *OrderRepository) SetAccept(ID uint) (ResponseOrder, error) {
+	var order ResponseOrder
+
+	if rowsAffected := repo.db.Table("orders").Where("id = ?", ID).Update("status", "accepted").RowsAffected; rowsAffected == 0 {
+		return ResponseOrder{}, errors.New("gagal mengubah status order menjadi accepted")
+	}
+
+	repo.db.Table("orders as o").Select("o.id as ID, o.user_id as UserID, o.service_id as ServiceID, s.service_title as ServiceTitle, s.price as Price, o.qty as Qty, o.date as Date, o.address as Address, o.city as City, o.phone as Phone, o.status as Status, o.is_paid as IsPaid, o.url as Url").
+		Joins("inner join services as s.id = o.service_id").
+		Joins("inner join payment_methods as pm on pm.id = o.payment_method_id").
+		Where("id = ?", ID).First(&order)
+	return order, nil
+}
+
+func (repo *OrderRepository) SetRejected(ID uint) (ResponseOrder, error) {
+	var order ResponseOrder
+
+	if rowsAffected := repo.db.Table("orders").Where("id = ?", ID).Update("status", "rejected").RowsAffected; rowsAffected == 0 {
+		return ResponseOrder{}, errors.New("gagal mengubah status order menjadi rejected")
+	}
+
+	repo.db.Table("orders as o").Select("o.id as ID, o.user_id as UserID, o.service_id as ServiceID, s.service_title as ServiceTitle, s.price as Price, o.qty as Qty, o.date as Date, o.address as Address, o.city as City, o.phone as Phone, o.status as Status, o.is_paid as IsPaid, o.url as Url").
+		Joins("inner join services as s.id = o.service_id").
+		Joins("inner join payment_methods as pm on pm.id = o.payment_method_id").
+		Where("id = ?", ID).First(&order)
+	return order, nil
+}
+
+func (repo *OrderRepository) SetCancel(ID uint) (ResponseOrder, error) {
+	var order ResponseOrder
+
+	if rowsAffected := repo.db.Table("orders").Where("id = ?", ID).Update("status", "cancel").RowsAffected; rowsAffected == 0 {
+		return ResponseOrder{}, errors.New("gagal mengubah status order menjadi cancel")
+	}
+
+	repo.db.Table("orders as o").Select("o.id as ID, o.user_id as UserID, o.service_id as ServiceID, s.service_title as ServiceTitle, s.price as Price, o.qty as Qty, o.date as Date, o.address as Address, o.city as City, o.phone as Phone, o.status as Status, o.is_paid as IsPaid, o.url as Url").
+		Joins("inner join services as s.id = o.service_id").
+		Joins("inner join payment_methods as pm on pm.id = o.payment_method_id").
+		Where("id = ?", ID).First(&order)
+	return order, nil
+}
+
+func (repo *OrderRepository) SetOnProcess(ID uint) (ResponseOrder, error) {
+	var order ResponseOrder
+
+	if rowsAffected := repo.db.Table("orders").Where("id = ?", ID).Update("status", "on process").RowsAffected; rowsAffected == 0 {
+		return ResponseOrder{}, errors.New("gagal mengubah status order menjadi on process")
+	}
+
+	repo.db.Table("orders as o").Select("o.id as ID, o.user_id as UserID, o.service_id as ServiceID, s.service_title as ServiceTitle, s.price as Price, o.qty as Qty, o.date as Date, o.address as Address, o.city as City, o.phone as Phone, o.status as Status, o.is_paid as IsPaid, o.url as Url").
+		Joins("inner join services as s.id = o.service_id").
+		Joins("inner join payment_methods as pm on pm.id = o.payment_method_id").
+		Where("id = ?", ID).First(&order)
+	return order, nil
+}
+
+func (repo *OrderRepository) SetDelivering(ID uint) (ResponseOrder, error) {
+	var order ResponseOrder
+
+	if rowsAffected := repo.db.Table("orders").Where("id = ?", ID).Update("status", "delivering").RowsAffected; rowsAffected == 0 {
+		return ResponseOrder{}, errors.New("gagal mengubah status order menjadi delivering")
+	}
+
+	repo.db.Table("orders as o").Select("o.id as ID, o.user_id as UserID, o.service_id as ServiceID, s.service_title as ServiceTitle, s.price as Price, o.qty as Qty, o.date as Date, o.address as Address, o.city as City, o.phone as Phone, o.status as Status, o.is_paid as IsPaid, o.url as Url").
+		Joins("inner join services as s.id = o.service_id").
+		Joins("inner join payment_methods as pm on pm.id = o.payment_method_id").
+		Where("id = ?", ID).First(&order)
+	return order, nil
+}
+
+func (repo *OrderRepository) SetDone(ID uint) (ResponseOrder, error) {
+	var order ResponseOrder
+
+	if rowsAffected := repo.db.Table("orders").Where("id = ?", ID).Update("status", "done").RowsAffected; rowsAffected == 0 {
+		return ResponseOrder{}, errors.New("gagal mengubah status order menjadi done")
+	}
+
+	repo.db.Table("orders as o").Select("o.id as ID, o.user_id as UserID, o.service_id as ServiceID, s.service_title as ServiceTitle, s.price as Price, o.qty as Qty, o.date as Date, o.address as Address, o.city as City, o.phone as Phone, o.status as Status, o.is_paid as IsPaid, o.url as Url").
+		Joins("inner join services as s.id = o.service_id").
+		Joins("inner join payment_methods as pm on pm.id = o.payment_method_id").
+		Where("id = ?", ID).First(&order)
+	return order, nil
+}

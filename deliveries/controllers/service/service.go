926d9bea9580ca160d39f992d8a3a8952c3e5867
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-14 16:46:36 +0700

    delete update image

--- deliveries/controllers/service/service.go
+++ deliveries/controllers/service/service.go
@@ -132,38 +132,6 @@ func (ctl *ServiceController) Update() echo.HandlerFunc {
 	}
 }
 
-func (ctl *ServiceController) UpdateImage() echo.HandlerFunc {
-	return func(c echo.Context) error {
-		isAlive := middlewares.ExtractTokenIsAlive(c)
-		if !isAlive {
-			return c.JSON(http.StatusUnauthorized, common.UnAuthorized("JWT token is expired"))
-		}
-
-		isAdmin := middlewares.ExtractTokenIsAdmin(c)
-		if !isAdmin {
-			return c.JSON(http.StatusUnauthorized, common.UnAuthorized("missing or malformed JWT"))
-		}
-
-		file, err := c.FormFile("file")
-		if err != nil {
-			log.Info(err)
-			return c.JSON(http.StatusBadRequest, common.BadRequest("tidak dapat membaca file gambar"))
-		}
-
-		image, err := uploader.Uploader(ctl.sess, ctl.config.S3_REGION, ctl.config.S3_BUCKET, *file)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, common.BadRequest(err.Error()))
-		}
-
-		ID, _ := strconv.Atoi(c.Param("id"))
-		res, err := ctl.repo.UpdateImage(uint(ID), image)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, common.InternalServerError(err.Error()))
-		}
-		return c.JSON(http.StatusCreated, common.Success(http.StatusCreated, "sukses memperbarui gambar service", ToResponseUpdate(res)))
-	}
-}
-
 func (ctl *ServiceController) Delete() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		isAlive := middlewares.ExtractTokenIsAlive(c)

6d28a0f4fee63927f53f84f9df335125eb7c59e7
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-16 01:18:12 +0700

    move mock repository to repository

--- repositories/mocks/order/order.go
+++ repositories/mocks/order/order.go
@@ -1,15 +1,15 @@
 package order
 
 import (
-	"final-project/entities/order"
+	O "final-project/entities/order"
 	"time"
 
 	"gorm.io/datatypes"
 )
 
-func OrderSeeder() order.Orders {
+func OrderSeeder() O.Orders {
 	date, _ := time.Parse("2006-01-02", "2022-03-11")
-	return order.Orders{
+	return O.Orders{
 		Qty:             1,
 		Date:            datatypes.Date(date),
 		Address:         "Jl. Soedirman No.13",

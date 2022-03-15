b30c6267d7908a4a411f0bb886b925a3aa5da795
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-15 21:28:48 +0700

    separate admin and user

--- deliveries/mocks/user/user.go
+++ deliveries/mocks/user/user.go
@@ -7,24 +7,30 @@ import (
 	"gorm.io/gorm"
 )
 
-type MockAuthRepository struct{}
+type MockAuthUserRepository struct{}
 
-func (repo *MockAuthRepository) Login(email, password string) (U.Users, error) {
-	return U.Users{Model: gorm.Model{ID: 1}, Email: "ucup@ucup.com", Password: "ucup123"}, nil
+func (repo *MockAuthUserRepository) Login(email, password string) (U.Users, error) {
+	return U.Users{Model: gorm.Model{ID: 1}, Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: false}, nil
+}
+
+type MockAuthAdminRepository struct{}
+
+func (repo *MockAuthAdminRepository) Login(email, password string) (U.Users, error) {
+	return U.Users{Model: gorm.Model{ID: 1}, Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: true}, nil
 }
 
 type MockUserRepository struct{}
 
 func (repo *MockUserRepository) Create(newUser U.Users) (U.Users, error) {
-	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Nur Wahid", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: true}, nil
+	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Nur Wahid", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: false}, nil
 }
 
 func (repo *MockUserRepository) Get(ID uint) (U.Users, error) {
-	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Nur Wahid", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: true}, nil
+	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Nur Wahid", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: false}, nil
 }
 
 func (repo *MockUserRepository) GetByID(ID uint) (U.Users, error) {
-	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Nur Wahid", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: true}, nil
+	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Nur Wahid", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: false}, nil
 }
 
 func (repo *MockUserRepository) GetAllUsers() ([]U.Users, error) {
@@ -33,7 +39,7 @@ func (repo *MockUserRepository) GetAllUsers() ([]U.Users, error) {
 		Name:     "Yusuf",
 		Email:    "ucup@ucup.com",
 		Password: "ucup123",
-		IsAdmin:  true,
+		IsAdmin:  false,
 	}
 
 	user2 := U.Users{
@@ -41,14 +47,14 @@ func (repo *MockUserRepository) GetAllUsers() ([]U.Users, error) {
 		Name:     "Frans",
 		Email:    "fransp@ucup.com",
 		Password: "frans123",
-		IsAdmin:  true,
+		IsAdmin:  false,
 	}
 
 	return []U.Users{user1, user2}, nil
 }
 
 func (repo *MockUserRepository) Update(userUpdate U.Users) (U.Users, error) {
-	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Updated", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: true}, nil
+	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Updated", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: false}, nil
 }
 
 func (repo *MockUserRepository) Delete(ID uint) error {
@@ -78,5 +84,73 @@ func (repo *MockFalseUserRepository) Update(userUpdate U.Users) (U.Users, error)
 }
 
 func (repo *MockFalseUserRepository) Delete(ID uint) error {
+	return errors.New("false delete")
+}
+
+type MockAdminRepository struct{}
+
+func (repo *MockAdminRepository) Create(newUser U.Users) (U.Users, error) {
+	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Nur Wahid", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: true}, nil
+}
+
+func (repo *MockAdminRepository) Get(ID uint) (U.Users, error) {
+	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Nur Wahid", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: true}, nil
+}
+
+func (repo *MockAdminRepository) GetByID(ID uint) (U.Users, error) {
+	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Nur Wahid", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: true}, nil
+}
+
+func (repo *MockAdminRepository) GetAllUsers() ([]U.Users, error) {
+	user1 := U.Users{
+		Model:    gorm.Model{ID: 1},
+		Name:     "Yusuf",
+		Email:    "ucup@ucup.com",
+		Password: "ucup123",
+		IsAdmin:  true,
+	}
+
+	user2 := U.Users{
+		Model:    gorm.Model{ID: 2},
+		Name:     "Frans",
+		Email:    "fransp@ucup.com",
+		Password: "frans123",
+		IsAdmin:  true,
+	}
+
+	return []U.Users{user1, user2}, nil
+}
+
+func (repo *MockAdminRepository) Update(userUpdate U.Users) (U.Users, error) {
+	return U.Users{Model: gorm.Model{ID: 1}, Name: "Yusuf Updated", Email: "ucup@ucup.com", Password: "ucup123", IsAdmin: true}, nil
+}
+
+func (repo *MockAdminRepository) Delete(ID uint) error {
+	return nil
+}
+
+type MockFalseAdminRepository struct{}
+
+func (repo *MockFalseAdminRepository) Create(newUser U.Users) (U.Users, error) {
+	return U.Users{}, errors.New("false create user")
+}
+
+func (repo *MockFalseAdminRepository) Get(ID uint) (U.Users, error) {
+	return U.Users{}, errors.New("false get user themselves")
+}
+
+func (repo *MockFalseAdminRepository) GetByID(ID uint) (U.Users, error) {
+	return U.Users{}, errors.New("false get user by id")
+}
+
+func (repo *MockFalseAdminRepository) GetAllUsers() ([]U.Users, error) {
+	return nil, errors.New("false get all users")
+}
+
+func (repo *MockFalseAdminRepository) Update(userUpdate U.Users) (U.Users, error) {
+	return U.Users{}, errors.New("false update user")
+}
+
+func (repo *MockFalseAdminRepository) Delete(ID uint) error {
 	return errors.New("false delete")
 }

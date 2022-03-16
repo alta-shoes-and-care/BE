bd5f740a912ae2b228887e45301ef68690354da1
fransihsan <fransihsan8888@gmail.com>
2022-03-16 12:23:40 +0700

    initial unit test payment method controller

---
+++ deliveries/controllers/payment-method/payment-method_test.go
@@ -0,0 +1,423 @@
+package paymentmethod
+
+import (
+	"bytes"
+	"encoding/json"
+	"final-project/deliveries/controllers/auth"
+	"final-project/deliveries/controllers/common"
+	"final-project/deliveries/middlewares"
+	MockUser "final-project/deliveries/mocks/user"
+	MockPM "final-project/deliveries/mocks/payment-method"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	e        = echo.New()
+	rootPath = "/payments"
+)
+
+func TestCreate(t *testing.T) {
+	var jwtTokenAdmin, jwtTokenUser string
+
+	if err := godotenv.Load(".env"); err != nil {
+		log.Info("tidak dapat memuat env file", err)
+	}
+
+	t.Run("login user", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthUserRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenUser = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenUser)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("login admin", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthAdminRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenAdmin = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenAdmin)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("fail admin", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(rootPath)
+
+		userController := NewPaymentMethodController(&MockPM.MockPaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Create())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusUnauthorized, response.Code)
+	})
+
+	t.Run("fail to bind json", func(t *testing.T) {
+		requestBody, _ := json.Marshal(RequestCreatePaymentMethod{
+			Name:  "",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		context := e.NewContext(req, res)
+		context.SetPath(rootPath)
+
+		userController := NewPaymentMethodController(&MockPM.MockPaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Create())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Nil(t, response.Data)
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+	})
+
+	t.Run("fail to create", func(t *testing.T) {
+		requestBody, _ := json.Marshal(RequestCreatePaymentMethod{
+			Name:     "klikbca",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+		context := e.NewContext(req, res)
+		context.SetPath(rootPath)
+
+		userController := NewPaymentMethodController(&MockPM.MockFalsePaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Create())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Nil(t, response.Data)
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+
+	t.Run("success to create", func(t *testing.T) {
+		requestBody, _ := json.Marshal(RequestCreatePaymentMethod{
+			Name:     "klikbca",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+		
+		context := e.NewContext(req, res)
+		context.SetPath(rootPath)
+
+		userController := NewPaymentMethodController(&MockPM.MockPaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Create())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.NotNil(t, response.Data)
+		assert.Equal(t, http.StatusCreated, response.Code)
+	})
+}
+
+func TestGet(t *testing.T) {
+	var jwtTokenAdmin, jwtTokenUser string
+	
+	if err := godotenv.Load(".env"); err != nil {
+		log.Info("tidak dapat memuat env file", err)
+	}
+
+	t.Run("login user", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthUserRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenUser = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenUser)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("login admin", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthAdminRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenAdmin = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenAdmin)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("fail admin", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(rootPath)
+
+		userController := NewPaymentMethodController(&MockPM.MockPaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Get())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusUnauthorized, response.Code)
+	})
+
+	t.Run("fail to get all users", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		context := e.NewContext(req, res)
+		context.SetPath(rootPath)
+
+		userController := NewPaymentMethodController(&MockPM.MockFalsePaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Get())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+
+	t.Run("succeed to get all users", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		context := e.NewContext(req, res)
+		context.SetPath(rootPath)
+
+		userController := NewPaymentMethodController(&MockPM.MockPaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Get())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	var jwtTokenUser, jwtTokenAdmin string
+
+	if err := godotenv.Load(".env"); err != nil {
+		log.Info("tidak dapat memuat env file", err)
+	}
+
+	t.Run("login user", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthUserRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenUser = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenUser)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("login admin", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthAdminRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenAdmin = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenAdmin)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("fail admin", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodDelete, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(rootPath)
+
+		userController := NewPaymentMethodController(&MockPM.MockPaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Delete())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusUnauthorized, response.Code)
+	})
+
+	t.Run("fail to delete", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodDelete, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/1", rootPath))
+
+		userController := NewPaymentMethodController(&MockPM.MockFalsePaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Delete())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+
+	t.Run("succeed to delete", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodDelete, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/1", rootPath))
+
+		userController := NewPaymentMethodController(&MockPM.MockPaymentMethodRepository{})
+		if err := middlewares.JWTMiddleware()(userController.Delete())(context); err != nil {
+			log.Fatal(err)
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+}

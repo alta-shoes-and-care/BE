94381f6e4ca9e01c66d2525ddb05c4cab36d094b
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-16 14:46:17 +0700

    delete done

--- deliveries/controllers/service/service_test.go
+++ deliveries/controllers/service/service_test.go
@@ -489,3 +489,131 @@ func TestGetDetails(t *testing.T) {
 		assert.Equal(t, http.StatusOK, response.Code)
 	})
 }
+
+func TestDelete(t *testing.T) {
+	var jwtTokenUser, jwtTokenAdmin string
+
+	t.Run("login user", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+		context.SetPath("/login")
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthUserRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenUser = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenUser)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("login admin", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+		context.SetPath("/login")
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthAdminRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenAdmin = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenAdmin)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("admin error", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v%v", rootPath, jwtPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewServiceController(&MockService.MockServiceTrueRepository{}, config, &MockService.MockAWSStructTrue{})
+		if err := middlewares.JWTMiddleware()(serviceController.Delete())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("internal server error", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v%v", rootPath, jwtPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewServiceController(&MockService.MockServiceFalseRepository{}, config, &MockService.MockAWSStructTrue{})
+		if err := middlewares.JWTMiddleware()(serviceController.Delete())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v%v", rootPath, jwtPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewServiceController(&MockService.MockServiceTrueRepository{}, config, &MockService.MockAWSStructTrue{})
+		if err := middlewares.JWTMiddleware()(serviceController.Delete())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+}

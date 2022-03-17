4c9de27ddd7c9702ab6f40da76e61e52c3b336c0
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-17 16:25:22 +0700

    remove update and delete review

--- repositories/review/interface.go
+++ repositories/review/interface.go
@@ -5,6 +5,4 @@ import R "final-project/entities/review"
 type Review interface {
 	Insert(newReview R.Reviews) (FormatReview, error)
 	Get() ([]FormatReview, error)
-	Update(reviewUpdate R.Reviews) (FormatReview, error)
-	Delete(ID, UserID uint) error
 }

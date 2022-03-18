d2f968711f2a563aef979881fffa0d72fd8e2d3a
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-18 22:57:05 +0700

    add return error

--- external/midtrans-pay/interface.go
+++ external/midtrans-pay/interface.go
@@ -3,6 +3,6 @@ package midtranspay
 import "github.com/midtrans/midtrans-go/coreapi"
 
 type MidtransClient interface {
-	CreateTransaction(userID, orderID, bill uint) *coreapi.ChargeResponse
+	CreateTransaction(userID, orderID, bill uint, payment string) (*coreapi.ChargeResponse, error)
 	CheckTransaction(userID, orderID uint) (string, error)
 }

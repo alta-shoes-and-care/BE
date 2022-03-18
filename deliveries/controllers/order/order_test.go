685a037b16e8a8ca5818fbc98169e00c743a1993
ynwahid <yusufnurwahid.contact@gmail.com>
2022-03-18 14:43:37 +0700

    check payment done

--- deliveries/controllers/order/order_test.go
+++ deliveries/controllers/order/order_test.go
@@ -617,3 +617,299 @@ func TestGetByID(t *testing.T) {
 		assert.Equal(t, http.StatusOK, response.Code)
 	})
 }
+
+func TestCheckPaymentStatus(t *testing.T) {
+	var jwtTokenUser, jwtTokenAdmin string
+
+	t.Run("login user", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+		context.SetPath("/login")
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthUserRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenUser = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenUser)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("login admin", func(t *testing.T) {
+		requestBody, _ := json.Marshal(auth.RequestLogin{
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		})
+
+		req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		context := e.NewContext(req, res)
+		context.SetPath("/login")
+
+		authControl := auth.NewAuthController(&MockUser.MockAuthAdminRepository{})
+		authControl.Login()(context)
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		dataMap := response.Data.(map[string]interface{})
+		jwtTokenAdmin = dataMap["token"].(string)
+
+		assert.NotEmpty(t, jwtTokenAdmin)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("internal midtrans error", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockTrueOrderRepository{}, &MockOrder.MockFalseMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+
+	t.Run("successful status pending", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockTrueOrderRepository{}, &MockOrder.MockTrueMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("internal error settlement", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockFalseOrderRepository{}, &MockOrder.MockSettlementMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+
+	t.Run("successful settlement", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockTrueOrderRepository{}, &MockOrder.MockSettlementMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("internal error cancel", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockFalseOrderRepository{}, &MockOrder.MockCancelMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+
+	t.Run("successful cancel", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockTrueOrderRepository{}, &MockOrder.MockCancelMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("internal error deny", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockFalseOrderRepository{}, &MockOrder.MockDenyMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+
+	t.Run("successful deny", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockTrueOrderRepository{}, &MockOrder.MockDenyMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("successful accept", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockTrueOrderRepository{}, &MockOrder.MockAcceptMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+
+	t.Run("successful challenge", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", bytes.NewBuffer(nil))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		context := e.NewContext(req, res)
+		context.SetPath(fmt.Sprintf("%v/check-payment", rootPath))
+		context.SetParamNames("id")
+		context.SetParamValues("1")
+
+		serviceController := NewOrderController(&MockOrder.MockTrueOrderRepository{}, &MockOrder.MockChallengeMidtrans{})
+		if err := middlewares.JWTMiddleware()(serviceController.CheckPaymentStatus())(context); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := common.Response{}
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+}
